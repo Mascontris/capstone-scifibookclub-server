447b897e2d067d3bf3615780a30517198f587ac3
Igor <Mascontris@yahoo.com>
2020-03-22 13:09:59 -0600

    funny change

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 
 const app = express()
 
@@ -12,12 +12,12 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors())
+app.use(cors({origin: CLIENT_ORIGIN}))
 app.use(helmet())
 
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.send('Hello, corona world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
